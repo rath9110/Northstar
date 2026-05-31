88c7b485f359b80097504ded8fe1e687d813e5a3
Rasmus Thunberg <rath701@gmail.com>
2026-05-31 21:39:00 +0200

    Added thinking around data points

---
+++ models.py
@@ -0,0 +1,4 @@
+""" "Happiness is "how happy did i feel today?", energy is "how much energy did I feel I had today?",
+stress is "was i feeling stressed or not?" have it be yes no instead of 1-10, friends/family time is if I hung out
+with them or not and then have notes be additional things I want to add such as "slept very late yesterday, drank,
+etc" """
