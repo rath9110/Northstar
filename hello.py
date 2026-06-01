8ddb3ad75edd4ea1ca980dc249dbaa512a11799b
Rasmus Thunberg <rath701@gmail.com>
2026-06-01 20:23:13 +0200

    Added model for db

--- hello.py
+++ hello.py
@@ -4,8 +4,21 @@ import sqlalchemy
 import psycopg2
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, Integer, Boolean, String, Date, Text
+from sqlalchemy.orm import declarative_base
 load_dotenv()
 connection_string = os.getenv("DATABASE_CONNECTION")
+
+base = declarative_base()
+
+class SomeClass(base):
+    __tablename__ = "some_table"
+    id = Column("Date", Date, primary_key=True)
+    happiness = Column("Happiness", Integer, nullable=False)
+    energy = Column("Energy", Integer, nullable=False)
+    stress = Column("Stress", Integer, nullable=False)
+    friends_family_time = Column("FriendsFamilyTime", Integer, nullable=False)
+    notes = Column("Notes", Text, nullable=True)
 
 app = fastapi.FastAPI()
 @app.get("/")
--- models.py
+++ models.py
@@ -2,3 +2,23 @@
 stress is "was i feeling stressed or not?" have it be yes no instead of 1-10, friends/family time is if I hung out
 with them or not and then have notes be additional things I want to add such as "slept very late yesterday, drank,
 etc" """
+
+""" 
+  1. What SQLAlchemy column type maps to each field — Integer, Boolean, String, Date, Text -> happiness: Integer, energy: Integer, stress: Boolean, friends/family time: Boolean, notes: Text
+  2. Which columns are nullable=False (required) vs nullable=True (optional) -> all are nullable false except notes which is nullable true
+  3. What the primary key is — think about what uniquely identifies a day  -> day_id that is a date and primary key
+"""
+
+from sqlalchemy import Column, Integer, Boolean, Date, Text
+from sqlalchemy.orm import declarative_base
+
+base = declarative_base()
+
+class DailyMood(base):
+    __tablename__ = "daily_mood"
+    date = Column("Date", Date, primary_key=True)
+    happiness = Column("Happiness", Integer, nullable=False)
+    energy = Column("Energy", Integer, nullable=False)
+    stressed = Column("Stressed", Boolean, nullable=False)
+    friends_family_time = Column("FriendsFamilyTime", Boolean, nullable=False)
+    notes = Column("Notes", Text, nullable=True)
