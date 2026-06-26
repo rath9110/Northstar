f5f83d9c03ab6afde798dd26c39f873f2c3e73db
Rasmus Thunberg <rath701@gmail.com>
2026-06-26 22:33:31 +0200

    Added separete weather table

--- models.py
+++ models.py
@@ -12,10 +12,12 @@ class DailyMood(Base):
     stressed = Column(Boolean, nullable=False)
     friends_family_time = Column(Boolean, nullable=False)
     notes = Column(Text, nullable=True)
-    __repr__ = f"<DailyMood(date={self.date}, happiness={self.happiness}, energy={self.energy}, stressed={self.stressed}, friends_family_time={self.friends_family_time}, notes={self.notes})>"
-
+    def __repr__(self):
+        return f"<DailyMood(date={self.date}, happiness={self.happiness}, energy={self.energy}, stressed={self.stressed}, friends_family_time={self.friends_family_time}, notes={self.notes})>"
+   
 class DailyAutomatedData(Base):
-    __tablename__ = "daily_automated_mood"
+    __tablename__ = "daily_automated_data"
     date = Column(Date, primary_key=True)
     weather_code = Column(String, nullable=True)
-    __repr__ = f"<DailyAutomatedData(date={self.date}, weather_code={self.weather_code})>"
+    def __repr__(self):
+        return f"<DailyAutomatedData(date={self.date}, weather_code={self.weather_code})>"
